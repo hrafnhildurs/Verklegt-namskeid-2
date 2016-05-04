922b7cd78ca9cdd9d0611ee3ce9324411750bb1d
hrafnhildur <hrafnhildurs15@ru.is>
2016-05-04 11:12:14 +0000

    hrafnhildursara: tom föll sett inn í projectController

--- Mooshak2.0/Controllers/AssignmentProjectController.cs
+++ Mooshak2.0/Controllers/AssignmentProjectController.cs
@@ -3,15 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mooshak2._0.Models;
+using Mooshak2._0.Models.ViewModels;
+using Mooshak2._0.DAL;
+using Mooshak2._0.Services;
 
 namespace Mooshak2._0.Controllers
 {
     public class AssignmentProjectController : Controller
     {
-        // GET: AssignmentProject
+        //instance of the AssignmentProjectService
+        private AssignmentProjectService _service = new AssignmentProjectService();
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(AssignmentProjectViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(AssignmentProjectViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(AssignmentProjectViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Details(AssignmentProjectViewModel model)
+        {
+            return View(model);
         }
     }
 }
