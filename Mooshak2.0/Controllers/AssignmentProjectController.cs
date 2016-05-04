e86c838e68d6b6104b2764347a2d982bfa85266e
Hrafnhildur Skúladóttir <hrafnhildursku15@ru.is>
2016-05-04 10:23:29 +0000

    allir klasar komnir inn

---
+++ Mooshak2.0/Controllers/AssignmentProjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooshak2._0.Controllers
+{
+    public class AssignmentProjectController : Controller
+    {
+        // GET: AssignmentProject
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mooshak2.0/Services/AssignmentProjectService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2._0.Services
+{
+    public class AssignmentProjectService
+    {
+    }
+}
