a4b67cdbc5f4483e6ffb7a4fe3f1d7630a4fb3aa
hrafnhildur <hrafnhildurs15@ru.is>
2016-05-06 13:17:18 +0000

    implement submit and export in AssProService

--- Mooshak2.0/Models/ViewModels/SubmissionViewModel.cs
+++ Mooshak2.0/Models/ViewModels/SubmissionViewModel.cs
@@ -8,7 +8,7 @@ namespace Mooshak2._0.Models.ViewModels
     public class SubmissionViewModel
     {
         public int AssignmentID { get; set; }
-        public int UserID { get; set; }
+        public int StudentID { get; set; }
         public string SubmittedCode { get; set; }
         public int ProjectID { get; set; }
         public string Date { get; set; }
