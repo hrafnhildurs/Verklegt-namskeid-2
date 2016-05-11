e17c4ff2f87b51b61c10a3e12ce24f53a76b8259
kolbrungun15 <kolbrungun15@ru.is>
2016-05-11 21:51:05 +0000

    Nú er hægt að senda inn textaskrá

--- Mooshak2.0/Controllers/SubmissionController.cs
+++ Mooshak2.0/Controllers/SubmissionController.cs
@@ -6,23 +6,38 @@ using System.Web.Mvc;
 using Mooshak2._0.Services;
 using Mooshak2._0.Models;
 using Mooshak2._0.Models.ViewModels;
+using System.IO;
 
 namespace Mooshak2._0.Controllers
 {
     public class SubmissionController : Controller
     {
+        private AssignmentProjectService _service = new AssignmentProjectService();
+
         [HttpGet]
         public ActionResult Submit()
         {
 
             return View();
         }
-
+        //Fékk þennann kóða hér : http://haacked.com/archive/2010/07/16/uploading-files-with-aspnetmvc.aspx/
         [HttpPost]
-        public ActionResult Submit(string code)
+        public ActionResult Submit(HttpPostedFileBase file)
         {
-            
-            return View();
+            if(file == null)
+            {
+                //TODO
+                //Kasta villu
+            }
+
+            else if(file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Submit");
         }
         public ActionResult Export(int StudentID, int ProjectID)
         {
