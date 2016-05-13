95132e6ba73a644595c61f8fac50f36269fbb388
hrafnhildur <hrafnhildurs15@ru.is>
2016-05-13 11:41:11 +0000

    created a classDiagram

--- Mooshak2.0/App_Start/FilterConfig.cs
+++ Mooshak2.0/App_Start/FilterConfig.cs
@@ -3,11 +3,4 @@ using System.Web.Mvc;
 
 namespace Mooshak2._0
 {
-    public class FilterConfig
-    {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-    }
 }
